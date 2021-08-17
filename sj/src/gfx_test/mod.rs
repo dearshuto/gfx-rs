338949a233f16b9ea0377e4e8208156f35e5c552
Shuto Shikama <>
2021-08-17 21:41:27 +0900

    レンダーステートのテストを追加

--- sj/src/gfx_test/mod.rs
+++ sj/src/gfx_test/mod.rs
@@ -1,4 +1,7 @@
+pub mod blend_state_api_test;
 pub mod buffer_api_test;
 pub mod command_buffer_api_test;
+pub mod depth_stencil_state_api_test;
 pub mod device_api_test;
 pub mod memory_pool_api_test;
+pub mod rasterizer_state_api_test;
