29c5d310c37281b5e2077722232e004450ba3f8d
Shuto Shikama <>
2021-08-20 00:45:44 +0900

    VertexState on wgpu

--- sj/src/gfx/wgpu/vertex_state_wgpu.rs
+++ sj/src/gfx/wgpu/vertex_state_wgpu.rs
@@ -1,10 +1,21 @@
+use crate::gfx::{VertexAttributeStateInfo, VertexBufferStateInfo};
+
 use super::super::vertex_state_api::{IVertexState, VertexStateInfo};
 use super::super::Device;
 
-pub struct VertexStateWgpu {}
+pub struct VertexStateWgpu {
+    _vertex_attribute_state_info: Vec<VertexAttributeStateInfo>,
+    _vertex_buffer_state_info: Vec<VertexBufferStateInfo>,
+}
 
 impl IVertexState for VertexStateWgpu {
-    fn new(_device: &Device, _info: &VertexStateInfo) -> Self {
-        todo!();
+    fn new(_device: &Device, info: &VertexStateInfo) -> Self {
+        let vertex_attribute_state_info_arra = info.get_attribute_state_info_array().to_vec();
+        let vertex_buffer_state_info_array = info.get_buffer_state_info_array().to_vec();
+
+        Self {
+            _vertex_attribute_state_info: vertex_attribute_state_info_arra,
+            _vertex_buffer_state_info: vertex_buffer_state_info_array,
+        }
     }
 }
