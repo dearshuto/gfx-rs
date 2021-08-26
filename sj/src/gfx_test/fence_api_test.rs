a52ab0a4a0bdb67b689c4b2d20da98405f94b4b7
Shuto Shikama <shuto@VH-AD3L>
2021-08-26 23:37:03 +0900

    Fence のテストを追加

--- sj/src/gfx_test/fence_api_test.rs
+++
@@ -1,8 +0,0 @@
-#[cfg(test)]
-use crate::gfx::{Device, DeviceInfo, Fence, FenceInfo};
-
-#[test]
-fn initialize() {
-    let device = Device::new(&DeviceInfo::new());
-    let _fence = Fence::new(&device, &FenceInfo {});
-}
--- sj/src/gfx_test/mod.rs
+++ sj/src/gfx_test/mod.rs
@@ -3,7 +3,6 @@ pub mod buffer_api_test;
 pub mod command_buffer_api_test;
 pub mod depth_stencil_state_api_test;
 pub mod device_api_test;
-pub mod fence_api_test;
 pub mod memory_pool_api_test;
 pub mod rasterizer_state_api_test;
 pub mod texture_api_test;
