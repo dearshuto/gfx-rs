5803849fc406078cd4ba75a1948281249327fb1e
Shuto Shikama <shuto@VH-AD3L>
2021-08-27 23:02:37 +0900

    単体テストの記述場所を整理

--- sj/src/gfx/depth_stencil_state_api.rs
+++ sj/src/gfx/depth_stencil_state_api.rs
@@ -50,3 +50,15 @@ impl<T: IDepthStencilStateImpl> TDepthStencilState<T> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::super::super::gfx::{DepthStencilState, DepthStencilStateInfo, Device, DeviceInfo};
+
+    #[test]
+    fn initialize() {
+        let device = Device::new(&DeviceInfo::new());
+        let depth_state_info = DepthStencilStateInfo::new();
+        let _depth_stencil_state = DepthStencilState::new(&device, &depth_state_info);
+    }
+}
