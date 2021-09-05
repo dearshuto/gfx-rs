e052959ca55352d0cfd61cae856a41a6776f0f63
Shuto Shikama <>
2021-09-05 10:43:57 +0900

    Test of ColorTargetView on Vulkano

--- sj/src/gfx/vk/color_target_view_vk.rs
+++ sj/src/gfx/vk/color_target_view_vk.rs
@@ -1,12 +1,21 @@
 use crate::gfx::color_target_view_api::{ColorTargetViewInfo, IColorTargetViewImpl};
 use crate::gfx::Device;
+use std::sync::Arc;
+use vulkano::image::view::{ImageView, ImageViewAbstract};
 
 pub struct ColorTargetViewVk<'a> {
     _device: &'a Device,
+    _image_view: Arc<dyn ImageViewAbstract>,
 }
 
 impl<'a> IColorTargetViewImpl<'a> for ColorTargetViewVk<'a> {
     fn new(device: &'a Device, info: &'a ColorTargetViewInfo) -> Self {
-        todo!()
+        let image_access = info.get_texture().to_data().clone_image_acess();
+        let image_view = ImageView::new(image_access).unwrap();
+
+        Self {
+            _device: device,
+            _image_view: image_view,
+        }
     }
 }
