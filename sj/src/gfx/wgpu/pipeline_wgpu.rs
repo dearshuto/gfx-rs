5690c7e7bfeb1da541fd833ff44e8d694b18eada
Shuto Shikama <>
2021-11-14 20:06:46 +0900

    Pipeline on wgpu の演算パイプラインの初期化をサポート

--- sj/src/gfx/wgpu/pipeline_wgpu.rs
+++ sj/src/gfx/wgpu/pipeline_wgpu.rs
@@ -5,41 +5,30 @@ use std::marker::PhantomData;
 pub struct Pipeline<'a> {
     render_pipeline_impl: Option<wgpu::RenderPipeline>,
     compute_pipeline_impl: Option<wgpu::ComputePipeline>,
-    bind_group: wgpu::BindGroup,
     _marker: PhantomData<&'a i32>,
 }
 
 impl<'a> IPipelineImpl<'a> for Pipeline<'a> {
-    fn new_as_graphics(device: &'a Device, info: &'a GraphicsPipelineInfo) -> Self {
+    fn new_as_graphics(_device: &'a Device, _info: &'a GraphicsPipelineInfo) -> Self {
         todo!()
     }
 
     fn new_as_compute(device: &'a Device, info: ComputePipelineInfo<'a>) -> Self {
-        let shader = info.get_shader().to_data().clone_compute_shader_module();
+        let shader_impl = info.get_shader().to_data();
+        let shader_module = shader_impl.clone_compute_shader_module();
+        let pipeline_layout = info.get_shader().to_data().get_pipeline_layout();
         let compute_pipeline = device.to_data().get_device().create_compute_pipeline(
             &wgpu::ComputePipelineDescriptor {
-                layout: None,
+                layout: Some(pipeline_layout),
                 label: None,
-                module: &shader,
+                module: &shader_module,
                 entry_point: &"main".to_string(),
             },
         );
 
-        let bind_group_layout = compute_pipeline.get_bind_group_layout(0);
-        let bind_group =
-            device
-                .to_data()
-                .get_device()
-                .create_bind_group(&wgpu::BindGroupDescriptor {
-                    label: None,
-                    layout: &bind_group_layout,
-                    entries: &[],
-                });
-
         Self {
             render_pipeline_impl: None,
             compute_pipeline_impl: Some(compute_pipeline),
-            bind_group,
             _marker: PhantomData,
         }
     }
@@ -56,10 +45,6 @@ impl<'a> Pipeline<'a> {
 
     pub fn get_compute_pipeline(&'a self) -> Option<&wgpu::ComputePipeline> {
         Some(self.compute_pipeline_impl.as_ref().unwrap())
-    }
-
-    pub fn get_bind_group(&self) -> &wgpu::BindGroup {
-        &self.bind_group
     }
 
     pub fn get_shader(&self) -> &Shader {
