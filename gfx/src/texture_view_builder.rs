80b1e86731e26e2aee0d3a946bce9b3774ec10a8
Shuto Shikama <@gmail.com>
2022-03-27 00:31:12 +0900

    TextureViewBuilder にフォーマット設定を追加

--- gfx/src/texture_view_builder.rs
+++ gfx/src/texture_view_builder.rs
@@ -1,19 +1,28 @@
-use sjgfx_interface::{ITextureView, TextureViewInfo};
+use sjgfx_interface::{ITextureView, ImageFormat, TextureViewInfo};
 
 use crate::api::IApi;
 
 pub struct TTextureViewBuilder<TApi: IApi> {
+    info: TextureViewInfo,
     _marker: std::marker::PhantomData<TApi>,
 }
 
 impl<TApi: IApi> TTextureViewBuilder<TApi> {
     pub fn new() -> Self {
         Self {
+            info: TextureViewInfo::new(),
             _marker: std::marker::PhantomData,
         }
     }
 
     pub fn build(&self, device: &TApi::Device, texture: &TApi::Texture) -> TApi::TextureView {
-        TApi::TextureView::new(device, &TextureViewInfo::new(), texture)
+        TApi::TextureView::new(device, &self.info, texture)
+    }
+
+    pub fn with_format(self, image_format: ImageFormat) -> Self {
+        Self {
+            info: self.info.set_format(image_format),
+            _marker: std::marker::PhantomData,
+        }
     }
 }
