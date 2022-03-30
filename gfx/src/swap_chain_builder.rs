96aeb60c520d55839aec44183eed8404cdb86716
Shuto Shikama <@gmail.com>
2022-03-31 01:54:57 +0900

    [gfx] SwapChainBuilder にサイズ設定を追加

--- gfx/src/swap_chain_builder.rs
+++ gfx/src/swap_chain_builder.rs
@@ -3,23 +3,33 @@ use sjgfx_interface::{ISwapChain, SwapChainInfo};
 use crate::api::IApi;
 
 pub struct TSwapChainBuilder<T: IApi> {
-    #[allow(dead_code)]
-    width: u32,
-    #[allow(dead_code)]
-    height: u32,
+    info: SwapChainInfo,
     _marker: std::marker::PhantomData<T>,
 }
 
 impl<T: IApi> TSwapChainBuilder<T> {
     pub fn new() -> Self {
         Self {
-            width: 640,
-            height: 480,
+            info: SwapChainInfo::new(),
             _marker: std::marker::PhantomData,
         }
     }
 
     pub fn build(&self, device: &mut T::Device) -> T::SwapChain {
-        T::SwapChain::new(device, &SwapChainInfo::new())
+        T::SwapChain::new(device, &self.info)
+    }
+
+    pub fn with_width(self, width: u32) -> Self {
+        Self {
+            info: self.info.with_width(width),
+            _marker: std::marker::PhantomData,
+        }
+    }
+
+    pub fn with_height(self, height: u32) -> Self {
+        Self {
+            info: self.info.with_height(height),
+            _marker: std::marker::PhantomData,
+        }
     }
 }
