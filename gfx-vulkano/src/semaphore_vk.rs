81c9abb8ee2243577ce868770e0f92ac440a1fc9
Shuto Shikama <@gmail.com>
2022-04-09 01:59:04 +0900

    [vk] gfx impl for SemaphoreVk

--- gfx-vulkano/src/semaphore_vk.rs
+++ gfx-vulkano/src/semaphore_vk.rs
@@ -1,1 +1,13 @@
-pub struct SemaphoreVk {}
+use sjgfx_interface::ISemaphore;
+
+use crate::DeviceVk;
+
+pub struct SemaphoreVk;
+
+impl ISemaphore for SemaphoreVk {
+    type DeviceType = DeviceVk;
+
+    fn new(device: &Self::DeviceType, _info: &sjgfx_interface::SemaphoreInfo) -> Self {
+        Self {}
+    }
+}
