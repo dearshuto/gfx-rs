805f05e135b965401970ed4abff969f341c414f7
Shuto Shikama <@gmail.com>
2022-05-20 00:31:48 +0900

    run_return の正しい使い方

--- gfx-egui/examples/egui_demo.rs
+++ gfx-egui/examples/egui_demo.rs
@@ -1,15 +1,26 @@
-use std::time::Instant;
+use std::{
+    thread::sleep,
+    time::{Duration, Instant},
+};
 
-use sjgfx::{api::IApi, TDeviceBuilder, TQueueBuilder, TSwapChainBuilder, TCommandBufferBuilder, TSemaphoreBuilder};
-use sjgfx_interface::{ISwapChain, ICommandBuffer, TextureArrayRange, IQueue};
-use winit::{event_loop::{EventLoop, ControlFlow}, window::WindowBuilder, platform::run_return::EventLoopExtRunReturn, dpi::PhysicalSize};
-use winit::event::Event::RedrawRequested;
-use winit::event::Event::WindowEvent;
-use winit::event::WindowEvent::Resized;
+use egui::FontDefinitions;
 use egui_winit_platform::Platform;
 use egui_winit_platform::PlatformDescriptor;
-use egui::FontDefinitions;
 use epi::App;
+use sjgfx::{
+    api::IApi, TCommandBufferBuilder, TDeviceBuilder, TQueueBuilder, TSemaphoreBuilder,
+    TSwapChainBuilder,
+};
+use sjgfx_interface::{ICommandBuffer, IQueue, ISwapChain, TextureArrayRange};
+use winit::event::Event::{RedrawRequested, self};
+use winit::event::Event::WindowEvent;
+use winit::event::WindowEvent::Resized;
+use winit::{
+    dpi::PhysicalSize,
+    event_loop::{ControlFlow, EventLoop},
+    platform::run_return::EventLoopExtRunReturn,
+    window::WindowBuilder,
+};
 
 struct ExampleRepaintSignal;
 
@@ -23,14 +34,19 @@ fn main() {
     run::<sjgfx::api::Wgpu>();
 }
 
-fn run<TApi: IApi>()
-{
+fn run<TApi: IApi>() {
     let mut event_loop = EventLoop::new();
-    let window = WindowBuilder::new().with_inner_size(PhysicalSize::new(2560, 1920)).build(&event_loop).unwrap();
+    let window = WindowBuilder::new()
+        .with_inner_size(PhysicalSize::new(2560, 1920))
+        .build(&event_loop)
+        .unwrap();
 
     let mut device = TDeviceBuilder::<TApi>::new().build_with_surface(&window, &event_loop);
     let mut queue = TQueueBuilder::<TApi>::new().build(&device);
-    let mut swap_chain = TSwapChainBuilder::<TApi>::new().with_width(2560).with_height(1920).build(&mut device);
+    let mut swap_chain = TSwapChainBuilder::<TApi>::new()
+        .with_width(2560)
+        .with_height(1920)
+        .build(&mut device);
     let mut command_buffer = TCommandBufferBuilder::<TApi>::new().build(&device);
     let mut semaphore = TSemaphoreBuilder::<TApi>::new().build(&device);
 
@@ -50,67 +66,87 @@ fn run<TApi: IApi>()
     // let repaint_signal = std::sync::Arc::new(ExampleRepaintSignal(std::sync::Mutex::new(
     //     event_loop.create_proxy(),
     // )));
-    let repaint_signal = std::sync::Arc::new(ExampleRepaintSignal{});
-    event_loop.run_return(|event, _, control_flow| {
-        platform.handle_event(&event);
+    let repaint_signal = std::sync::Arc::new(ExampleRepaintSignal {});
+    let mut quit = false;
+    while !quit {
+        event_loop.run_return(|event, _, control_flow| {
+            platform.handle_event(&event);
 
-        *control_flow = ControlFlow::Poll;
+            *control_flow = ControlFlow::Wait;
 
-        window.request_redraw();
-        match event {
-            RedrawRequested(..) => {
-                // 画面をクリア
-                let mut next_scan_buffer_view = swap_chain.acquire_next_scan_buffer_view(Some(&mut semaphore), None/*fence*/);
-                command_buffer.begin();
-                command_buffer.clear_color(&mut next_scan_buffer_view, 0.1, 0.2, 0.4, 0.0, TextureArrayRange::new());
-                command_buffer.end();
-                queue.execute(&command_buffer);
+            match event {
+                RedrawRequested(..) => {
+                    // 画面をクリア
+                    let mut next_scan_buffer_view = swap_chain
+                        .acquire_next_scan_buffer_view(Some(&mut semaphore), None /*fence*/);
+                    command_buffer.begin();
+                    command_buffer.clear_color(
+                        &mut next_scan_buffer_view,
+                        0.1,
+                        0.2,
+                        0.4,
+                        0.0,
+                        TextureArrayRange::new(),
+                    );
+                    command_buffer.end();
+                    queue.execute(&command_buffer);
 
-                platform.update_time(start_time.elapsed().as_secs_f64());
+                    platform.update_time(start_time.elapsed().as_secs_f64());
 
-                let egui_start = Instant::now();
-                platform.begin_frame();
-                let app_output = epi::backend::AppOutput::default();
+                    let egui_start = Instant::now();
+                    platform.begin_frame();
+                    let app_output = epi::backend::AppOutput::default();
 
-                let mut frame =  epi::Frame::new(epi::backend::FrameData {
-                    info: epi::IntegrationInfo {
-                        name: "egui_example",
-                        web_info: None,
-                        cpu_usage: previous_frame_time,
-                        native_pixels_per_point: Some(window.scale_factor() as _),
-                        prefer_dark_mode: None,
-                    },
-                    output: app_output,
-                    repaint_signal: repaint_signal.clone(),
-                });
+                    let mut frame = epi::Frame::new(epi::backend::FrameData {
+                        info: epi::IntegrationInfo {
+                            name: "egui_example",
+                            web_info: None,
+                            cpu_usage: previous_frame_time,
+                            native_pixels_per_point: Some(window.scale_factor() as _),
+                            prefer_dark_mode: None,
+                        },
+                        output: app_output,
+                        repaint_signal: repaint_signal.clone(),
+                    });
 
-                // Draw the demo application.
-                demo_app.update(&platform.context(), &mut frame);
-                let (_output, paint_commands) = platform.end_frame(Some(&window));
-                let paint_jobs = platform.context().tessellate(paint_commands);
+                    // Draw the demo application.
+                    demo_app.update(&platform.context(), &mut frame);
+                    let (_output, paint_commands) = platform.end_frame(Some(&window));
+                    let paint_jobs = platform.context().tessellate(paint_commands);
 
-                let frame_time = (Instant::now() - egui_start).as_secs_f64() as f32;
-                previous_frame_time = Some(frame_time);
+                    let frame_time = (Instant::now() - egui_start).as_secs_f64() as f32;
+                    previous_frame_time = Some(frame_time);
 
-                gfx_egui_render_pass.update_buffers(&device, &paint_jobs);
-                gfx_egui_render_pass.update_texture(&device, &platform.context().font_image());
-                gfx_egui_render_pass.execute(&next_scan_buffer_view, &mut queue, &paint_jobs, window.scale_factor() as f32);
+                    gfx_egui_render_pass.update_buffers(&device, &paint_jobs);
+                    gfx_egui_render_pass.update_texture(&device, &platform.context().font_image());
+                    gfx_egui_render_pass.execute(
+                        &next_scan_buffer_view,
+                        &mut queue,
+                        &paint_jobs,
+                        window.scale_factor() as f32,
+                    );
 
-                queue.present(&mut swap_chain);
+                    queue.present(&mut swap_chain);
 
-                queue.flush();
-                queue.sync();
-            }
-            WindowEvent { event, .. } => match event {
-                Resized(_size) => {
+                    queue.flush();
+                    queue.sync();
                 }
-                winit::event::WindowEvent::CloseRequested => {
+                Event::MainEventsCleared => {
                     *control_flow = ControlFlow::Exit;
                 }
+                WindowEvent { event, .. } => match event {
+                    Resized(_size) => {}
+                    winit::event::WindowEvent::CloseRequested => {
+                        *control_flow = ControlFlow::Exit;
+                        quit = true;
+                    }
+                    _ => {}
+                },
                 _ => {}
             }
-            _ => {}
-        }
-    });
+        });
 
+        window.request_redraw();
+        sleep(Duration::from_millis(16));
+    }
 }
