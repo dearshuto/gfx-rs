7d65c515c1a3d92579d3593967db964cc256d3b2
Shuto Shikama <>
2022-07-10 16:51:45 +0900

    [glow] Sampler

--- gfx-glow/src/lib.rs
+++ gfx-glow/src/lib.rs
@@ -4,6 +4,7 @@ mod buffer_glow;
 mod command_buffer_glow;
 mod device_glow;
 mod queue_glow;
+mod sampler_glow;
 mod shader_glow;
 pub mod vi;
 
@@ -11,6 +12,7 @@ pub use buffer_glow::BufferGlow;
 pub use command_buffer_glow::CommandBufferGlow;
 pub use device_glow::DeviceGlow;
 pub use queue_glow::QueueGlow;
+pub use sampler_glow::SamplerGlow;
 pub use shader_glow::ShaderGlow;
 
 static mut GLOW_STATIC_DATA: Option<StaticData> = None;
