1459e8e4b9d59fee72092e753bac643ec0e55147
Shuto Shikama <>
2022-07-10 15:39:49 +0900

    [WIP] glow の Buffer を実装中

--- gfx-glow/src/buffer_glow.rs
+++ gfx-glow/src/buffer_glow.rs
@@ -1,3 +1,4 @@
+use sjgfx_interface::IBuffer;
 use std::sync::Arc;
 
 use glow::HasContext;
@@ -25,4 +26,34 @@ impl BufferGlow {
     pub fn get_handle(&self) -> glow::NativeBuffer {
         self.buffer
     }
+}
+
+impl IBuffer for BufferGlow {
+    type DeviceType = DeviceGlow;
+
+    fn new(device: &Self::DeviceType, info: &BufferInfo) -> Self {
+        BufferGlow::new(device, info)
+    }
+
+    fn map<T, F: Fn(&T)>(&self, func: F) {
+        self.map(func);
+    }
+
+    fn map_mut<T, F: Fn(&mut T)>(&self, func: F) {
+        self.map_mut(func);
+    }
+
+    fn map_as_slice<T, F: Fn(&[T])>(&self, func: F) {
+        let size = self.size / std::mem::size_of::<T>();
+        self.map_as_slice(size + 1, func);
+    }
+
+    fn map_as_slice_mut<T, F: Fn(&mut [T])>(&self, func: F) {
+        let size = self.size / std::mem::size_of::<T>();
+        self.map_as_slice_mut(size, func);
+    }
+
+    fn flush_mapped_range(&self, _offset: isize, _size: usize) {}
+
+    fn invalidate_mapped_range(&self, _offset: isize, _size: usize) {}
 }
