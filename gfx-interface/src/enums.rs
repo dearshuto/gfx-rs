e90b9adb3b2b90ae35aedcba5187fcd7a0facb8d
Shuto Shikama <@gmail.com>
2023-01-11 03:19:34 +0900

    enum 定義がフラグ状態になってなかったので修正

--- gfx-interface/src/enums.rs
+++ gfx-interface/src/enums.rs
@@ -2,17 +2,17 @@ use bitflags::bitflags;
 
 bitflags! {
     pub struct GpuAccess: u32 {
-        const VERTEX_BUFFER = 0x01;
-        const INDEX_BUFFER = 0x02;
-        const CONSTANT_BUFFER = 0x04;
+        const VERTEX_BUFFER = 1;
+        const INDEX_BUFFER = 1 << 1;
+        const CONSTANT_BUFFER = 1 << 2;
         const TEXTURE = 0x08;
-        const UNORDERED_ACCESS_BUFFER = 0x16;
-        const COLOR_BUFFER = 0x32;
-        const DEPTH_STENCIL = 0x64;
-        const READ = 0x128;
-        const WRITE = 0x256;
-        const INDIRECT_BUFFER = 0x128;
-        const IMAGE = 0x4000;
+        const UNORDERED_ACCESS_BUFFER = 1 << 3;
+        const COLOR_BUFFER = 1 << 4;
+        const DEPTH_STENCIL = 1 << 5;
+        const READ = 1 << 6;
+        const WRITE = 1 << 7;
+        const INDIRECT_BUFFER = 1 << 8;
+        const IMAGE = 1 << 9;
     }
 }
 
