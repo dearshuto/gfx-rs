49b00041fa9aa06a8b9091afb1832b2ecbfd9a20
Shuto Shikama <shuto.shikama@gmail.com>
2025-07-22 17:25:57 +0900

    [WIP] Vulkan API への抽象化レイヤーを追加

---
+++ vulkapi/src/lib.rs
@@ -0,0 +1,92 @@
+use ash::prelude::VkResult;
+
+pub trait Interface {
+    type Entry: Entry;
+}
+
+pub trait Entry {
+    type Instance;
+
+    fn loaded() -> Self;
+
+    fn instance(&self) -> Self::Instance;
+}
+
+pub trait Instance {
+    type PhysicalDevice: Copy;
+    type Device: Device;
+
+    fn enumerate_physical_devices(&self) -> Result<Vec<Self::PhysicalDevice>, ash::vk::Result>;
+
+    fn get_physical_device_queue_family_properties(
+        &self,
+        physical_device: Self::PhysicalDevice,
+    ) -> Vec<ash::vk::QueueFamilyProperties>;
+
+    fn create_device(
+        physical_device: ash::vk::PhysicalDevice,
+        create_info: &ash::vk::DeviceCreateInfo,
+        allocator: Option<&ash::vk::AllocationCallbacks>,
+    ) -> Result<Self::Device, ash::vk::Result>;
+}
+
+pub trait Device {
+    type Queue: Copy;
+    type CommandPool;
+    type CommandBuffer: Copy;
+    type ImageView;
+    type RenderPass: Copy;
+    type Pipeline: Copy;
+    type Semaphore: Copy;
+    type Fence: Copy;
+
+    fn get_device_queue(&self, queue_family_index: u32, queue_index: u32) -> Self::Queue;
+
+    fn create_command_pool(
+        &self,
+        create_info: &ash::vk::CommandPoolCreateInfo,
+        allocator: Option<&ash::vk::AllocationCallbacks>,
+    ) -> Self::CommandPool;
+
+    fn allocate_command_buffers(
+        &self,
+        allocate_info: &ash::vk::CommandBufferAllocateInfo,
+    ) -> Result<Vec<Self::CommandPool>, ash::vk::Result>;
+
+    fn create_image_view(
+        &self,
+        create_info: &ash::vk::ImageCreateInfo,
+        allocator: Option<&ash::vk::AllocationCallbacks>,
+    ) -> Result<Vec<Self::ImageView>, ash::vk::Result>;
+
+    // 描画コマンド
+
+    fn cmd_begin_render_pass(
+        &self,
+        command_buffer: Self::CommandBuffer,
+        begin_info: &ash::vk::RenderPassBeginInfo,
+        contents: ash::vk::SubpassContents,
+    );
+
+    fn cmd_bind_pipeline(
+        &self,
+        command_buffer: Self::CommandBuffer,
+        bind_point: ash::vk::PipelineBindPoint,
+        pipeline: Self::Pipeline,
+    );
+
+    fn cmd_draw(&self);
+
+    fn cmd_end_render_pass(&self, command_buffer: Self::CommandBuffer);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
